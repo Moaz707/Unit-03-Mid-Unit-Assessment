442c1b7ddd064c59a05f1c326f11b31ae480cba4
Muaadh Melhi <Muaadhmelhi@ac.c4q.nyc>
2017-11-21 20:25:14 -0500

    Seconed

--- app/src/main/java/nyc/muaadh_melhi_develpoer/whatismyaddress/MainActivity.java
+++ app/src/main/java/nyc/muaadh_melhi_develpoer/whatismyaddress/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button save, move;
+    String emailAddress;
     private static final String address_shared_preferences = "sharedPrefsTesting";
     private SharedPreferences sharedPreferences;
 
@@ -31,24 +33,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("email address", editText.getText().toString());
+                emailAddress = editText.getText().toString();
+                editor.putString(emailAddress, emailAddress);
+
                 editor.commit();
+                Log.e(emailAddress, sharedPreferences.getString(emailAddress, ""));
                 editText.setText("");
 
             }
         });
 
 
-
-
-     move.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             Intent intent = new Intent(MainActivity.this, RecyclerActivity.class);
-             intent.putExtra("sharedpref", editText.getText().toString());//check again if that what they want
-             startActivity(intent);
-         }
-     });
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RecyclerActivity.class);
+                intent.putExtra("sharedpref", editText.getText().toString());//check again if that what they want
+                intent.putExtra("sharedKey", address_shared_preferences);
+                startActivity(intent);
+            }
+        });
     }
 }
 
