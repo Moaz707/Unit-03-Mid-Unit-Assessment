1a47c2fb6b0a7aca826c337696cc5933fcf21d84
Muaadh Melhi <Muaadhmelhi@ac.c4q.nyc>
2017-11-21 20:19:46 -0500

    change the value of the shared as jose said

--- app/src/main/java/nyc/muaadh_melhi_develpoer/whatismyaddress/RecyclerActivity.java
+++ app/src/main/java/nyc/muaadh_melhi_develpoer/whatismyaddress/RecyclerActivity.java
@@ -1,13 +1,21 @@
 package nyc.muaadh_melhi_develpoer.whatismyaddress;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Map;
 
 public class RecyclerActivity extends AppCompatActivity {
+
+    private SharedPreferences registerPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+
     }
 }
